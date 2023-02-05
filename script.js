27bc7f1ab542b3df2aa3ddc93f59e6469199b4ff
Nikhil <nikhil.neon@gmail.com>
2023-02-05 09:11:23 +0530

    updated gitignore

--- script.js
+++ script.js
@@ -14,40 +14,54 @@ const winCombinations = [
 let xTurn = true;
 let gameProgress = 0;
 let result = null;
-const grid = Array(9).fill(null);
 let message = null;
+const grid = Array(9).fill(null);
 
-const gameGrid = document.querySelector(".game-grid");
-const buttons = gameGrid.querySelectorAll("button");
 const progressBar = document.querySelector("#game-progress");
 const messageBar = document.querySelector(".alert");
 
+function MakeGrid() {
+  const gameGrid = document.querySelector(".game-grid");
+  grid.forEach((element, index) => {
+    const btn = document.createElement("button");
+    gameGrid.appendChild(btn);
+    btn.id = index;
+  });
+
+  return {
+    grid,
+    gameGrid,
+  };
+}
+
 function startGame() {
+  grid.forEach((element, index) => {
+    grid[index] = null;
+  });
   // Main function
-  messageBar.style.display = "none";
+  const buttons = myGrid.gameGrid.querySelectorAll("button");
   buttons.forEach((button) => {
-    // Handles click, if not win/ tie, switches player else returns win message
+    // Handles click, if not win / tie, switches player else returns win message
     button.addEventListener("click", () => {
+      messageBar.style.display = "none";
       // eslint-disable-next-line no-use-before-define
       handleClick(button);
-
       // check for win condition
       // eslint-disable-next-line no-use-before-define
       checkWinCondition();
-      if (result === X) {
-        message = `Player ${result} won the game!`;
-      } else if (result === O) {
+      if (result === X || result === O) {
         message = `Player ${result} won the game!`;
       } else if (result === "tie") {
         message = "It's a Tie!";
       }
-
       if (message) {
         messageBar.innerHTML = message;
+        console.log(message);
         messageBar.style.display = "block";
+        resetGame();
+      } else {
+        button.setAttribute("disabled", true);
       }
-
-      button.setAttribute("disabled", true);
     });
 
     // Handling mouseover action
@@ -62,7 +76,6 @@ function startGame() {
       handleHover(button, false);
     });
   });
-  // end of start game
 }
 
 // function to switch turns on click
@@ -79,6 +92,7 @@ function handleClick(e) {
   const currentPlayer = xTurn ? X : O; // Returns X or O depending on the player who clicks
   gameProgress += 1;
   progressBar.value = gameProgress;
+  messageBar.style.display = "";
   // update the grid to current player's mark
   e.innerHTML = currentPlayer;
   // get the id of the cell which is also the grid array index & update the grid with player sign
@@ -95,7 +109,7 @@ function handleHover(e, action) {
   } else { e.innerHTML = ""; }
 }
 
-// takes a grid and checks if there is a win condition
+// Checks if there is a win condition in the grid.
 function checkWinCondition() {
   for (let i = 0; i < winCombinations.length; i += 1) {
     const [a, b, c] = winCombinations[i];
@@ -109,4 +123,26 @@ function checkWinCondition() {
   }
 }
 
+const myGrid = MakeGrid(grid);
 startGame();
+
+function resetGame() {
+  // Reset all buttons
+  const gameGrid = document.querySelector(".game-grid");
+  const buttons = gameGrid.querySelectorAll("button");
+  buttons.forEach((btn) => {
+    btn.innerHTML = "";
+    btn.removeAttribute("disabled");
+  });
+
+  // reset the grid array
+  grid.forEach((element, index) => {
+    grid[index] = null;
+  });
+
+  // do something
+  xTurn = true;
+  gameProgress = 0;
+  result = null;
+  message = null;
+}
